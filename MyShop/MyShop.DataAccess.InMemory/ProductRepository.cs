3c52b145ba6d90e2222b0d78a782ec6c0547bf2f
Jorge Barrientos <jorge.barrientos@gmail.com>
2019-05-05 16:55:26 -0600

    Product Model and Inmemory Product Repository Built.

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,83 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+
+            if (products == null)
+                products = new List<Product>();
+        }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(f => f.Id == product.Id);
+
+            if(productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product not found.");
+            }
+        }
+
+        public Product Find(string Id)
+        {
+
+            Product product = products.Find(f => f.Id == Id);
+
+            if (product  != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found.");
+            }
+
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Product product = products.Find(f => f.Id == Id);
+
+            if (product != null)
+            {
+                products.Remove(product);
+            }
+            else
+            {
+                throw new Exception("Product not found.");
+            }
+        }
+    }
+}
